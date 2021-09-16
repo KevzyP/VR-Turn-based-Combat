1c0db5989bd087612a5492f22296e44ac8479573
PRIMESKILLS-POP\Kevz <keytotruth12@gmail.com>
2021-09-16 17:20:38 +0700

    16-09-2021

---
+++ Assets/Scripts/Hand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    private Animator _animator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void WeaponGrab()
+    {
+        _animator.SetBool("IsGrabbed", true);
+    }
+    public void WeaponRelease()
+    {
+        _animator.SetBool("IsGrabbed", false);
+    }
+
+    
+}
---
+++ Assets/Scripts/RotateMenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateMenuController : MonoBehaviour
+{
+    public RotateActions rotateActions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void EnableRotatingActions()
+    {
+        rotateActions.gameObject.SetActive(true);
+    }
+
+    public void DisableRotatingActions()
+    {
+        rotateActions.gameObject.SetActive(false);
+    }
+
+}
